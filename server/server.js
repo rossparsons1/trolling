18fe800c6899ebf98436e33fbc5d495c20721743
rossparsons1 <114895449+rossparsons1@users.noreply.github.com>
2024-11-29 10:46:11 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://rossparsons1.github.io'
+}))
 app.use(express.json());
 
 
