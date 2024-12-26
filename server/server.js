cbb30d77fbcb896aae927c19cf445390cb01d049
rossparsons1 <114895449+rossparsons1@users.noreply.github.com>
2024-12-26 11:44:54 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,14 +16,15 @@ app.use(express.json());
 app.post('/', async (req,res) => {
     const text = req.body.data;
     console.log(text)
-    try {
-        const response = await axios.post('https://trollingbot.onrender.com/predict/', {
-            text: text,
-        });
+    axios.post('https://trollingbot.onrender.com/predict/', {
+        text: text,
+    })
+    .then(response => {
         res.json(response.data);
-    } catch (error) {
+    })
+    .catch(error => {
         res.status(500).send(error);
-    }
+    });
 })
 
 app.listen(8080, () => {
